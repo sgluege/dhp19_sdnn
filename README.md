a1402f05b447573c66041d6b1780fab38f9788ce
Stefan Glüge <glue@zhaw.ch>
2023-08-21 09:59:34 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,102 @@
 # dhp19_sdnn
 Repo to debug lava-dl SDNN training pipeline for DHP19 data ...
+
+# Sample data
+Download the sample data at https://drive.switch.ch/index.php/s/9aICDpDuVmoH4Bs and store it in the `data` folder.
+
+## Data format
+
+### Inputs / Event frames
+A single input is an event frame of 260x344 pixels. Each scene was recored from 4 cameras. We present 8 cosecutive event frames to the SDNN. With a batch size of 8, the input shape is thus `[8, 1, 260, 344, 4, 8]`: 8 batches, 1 channel, 260x344 pixels, 4 cameras, 8 time steps.
+
+During training (yet) we chose a single camera to train on. Thus, the input to the model is `[8, 1, 260, 344, 8]`: 8 batches, 1 channel, 260x344 pixels, 8 time steps.
+```
+input.shape()   # torch.Size([8, 1, 260, 344, 4, 8])
+```
+
+### Targets / Labels
+The target for each joint is a single frame of 260x344 pixels. We have labels for 13 joints. We present 8 cosecutive event frames to the SDNN. With a batch size of 8, the target shape is thus `[8, 13, 260, 344, 4, 8]`: 8 batches, 13 channels/joints, 260x344 pixels, 4 cameras, 8 time steps.
+
+During training (yet) we chose a single camera to train on. Thus, the target for the model is `[8, 13, 260, 344, 8]`: 8 batches, 13 channels/joints, 260x344 pixels, 8 time steps.
+
+```
+target.shape # torch.Size([8, 13, 260, 344, 4, 8])
+```
+
+![Sample input with single target joint](doc/img/input_sample.png "Input sample with single target joint")
+
+# Requirements
+The code was tested with Python 3.8.10 and the following packages:
+- lava_nc-0.7.0.tar.gz
+- lava_dl-0.3.3.tar.gz
+- torch 1.13.1+cu117
+
+Running in the nvidia/cuda:11.6.2-devel-ubuntu20.04 docker container.
+
+# Model
+The proposed CNN [1] has 17 convolutional layers. Each layer has 3x3 ﬁlter size and is followed by Rectiﬁed Linear Unit (ReLU) activation. The DVS resolution is used as the CNN input resolution, and it is decreased with two max pooling layers in the ﬁrst stages of the network. Then, it is recovered in later stages with two transposed convolution layers with stride 2. The convolutional layers of the network do not include biases. This architectural choice was motivated by an increase in the activation sparsity at a negligible decrease in performance. As discussed in Sec. 6, activation sparsity could be exploited for faster processing. The CNN has about 220k parameters and requires 6.2 GOp/frame, where one Op is one multiplication or addition. In designing the CNN, we paid attention both to its prediction accuracy and computational complexity, to minimize model size for real-time applications.
+
+CNN architecture details. Changes in spatial resolution (Res.) are due to 2x2 max pooling (MP) or transposed convolution (TC) with stride 2. Dilation refers to the dilation rate in convolutional layers. The input is a constant-count DVS frame with shape 344x260x1.
+| Layer     | 1   | 2   | 3   | 4  | 5  | 6  | 7  | 8  | 9   | 10  | 11  | 12  | 13  | 14  | 15  | 16  | 17  |
+|-----------|-----|-----|-----|----|----|----|----|----|-----|-----|-----|-----|-----|-----|-----|-----|-----|
+| Stride    | 1   | 1   | 1   | 1  | 1  | 1  | 1  | 1  | 2   | 1   | 1   | 1   | 1   | 2   | 1   | 1   | 1   |
+| Dilation  | 1   | 1   | 1   | 1  | 2  | 2  | 2  | 2  | 1   | 2   | 2   | 2   | 2   | 1   | 1   | 1   | 1   |
+| Res.      | MP  |     |     | MP |    |    |    |    | TC  |     |     |     |     | TC  |     |     |     |
+| Output ch | 16  | 32  | 32  | 32 | 64 | 64 | 64 | 64 | 32  | 32  | 32  | 32  | 32  | 16  | 16  | 16  | 13  |
+| Output H  | 130 | 130 | 130 | 65 | 65 | 65 | 65 | 65 | 130 | 130 | 130 | 130 | 130 | 260 | 260 | 260 | 260 |
+| Output W  | 172 | 172 | 172 | 86 | 86 | 86 | 86 | 86 | 172 | 172 | 172 | 172 | 172 | 344 | 344 | 344 | 344 |
+
+The CNN model predicts a set of 2D heatmaps, representing the probability of the joint presence at each pixel location, as proposed in [2].
+
+[1] Calabrese, E., Taverni, G., Easthope, C. A., Skriabine, S., Corradi, F., Longinotti, L., Eng, K., & Delbruck, T. (n.d.). DHP19: Dynamic Vision Sensor 3D Human Pose Dataset. https://sites.google.com/view/dhp19.
+
+[2] Jonathan J. Tompson, Arjun Jain, Yann LeCun, and Christoph Bregler. Joint training of a convolutional network and a graphical model for human pose estimation. In Advances in Neural Information Processing Systems (NIPS), pages 1799–1807, 2014
+https://github.com/max-andr/joint-cnn-mrf
+
+We tried to implement the model as described in the paper as SDNN using lava-dl. However, we are not sure if we got it right. The model is implemented in `dhp19_sdnn_model.py`.
+
+# Problem
+No learning happens whatsoever :/. Running `dhp19_sdnn_train.py` results in the following output:
+
+```
+Epoch [0/10]: 100%|██████████| 24/24 [00:39<00:00,  1.63s/it]
+Loss: 0.0003  
+Epoch [1/10]: 100%|██████████| 24/24 [00:37<00:00,  1.55s/it]
+Loss: 0.0003  
+Epoch [2/10]: 100%|██████████| 24/24 [00:37<00:00,  1.58s/it]
+Loss: 0.0003  
+Epoch [3/10]: 100%|██████████| 24/24 [00:37<00:00,  1.56s/it]
+Loss: 0.0003  
+Epoch [4/10]: 100%|██████████| 24/24 [00:37<00:00,  1.57s/it]
+Loss: 0.0003  
+Epoch [5/10]: 100%|██████████| 24/24 [00:37<00:00,  1.55s/it]
+Loss: 0.0003  
+Epoch [6/10]: 100%|██████████| 24/24 [00:37<00:00,  1.56s/it]
+Loss: 0.0003  
+Epoch [7/10]: 100%|██████████| 24/24 [00:38<00:00,  1.58s/it]
+Loss: 0.0003  
+Epoch [8/10]: 100%|██████████| 24/24 [00:36<00:00,  1.54s/it]
+Loss: 0.0003  
+Epoch [9/10]: 100%|██████████| 24/24 [00:36<00:00,  1.51s/it]
+Loss: 0.0003  
+```
+
+# Vanishing gradients?
+`dhp19_sdnn_train_check_grads.py` outputs the gradients for each layer for the first two minibatches.
+
+Looking at the gradients, they seem to be vanishing. The gradients are all very small, even for the first layer. The gradients for the first layer are all in the range of 1e. The gradients for the last layer are in the range of 1e-8. The gradients for the layers in between are in 
+the range of 1e-6 to 1e-8... For the first minibatch.
+
+For the second minibatch the gradients a basically 0 for all layers above the second layer.
+
+![Gradients for the first minibatch](doc/img/grads_per_layer_step1.png "Gradients for the first minibatch")
+![Gradients for the second minibatch](doc/img/grads_per_layer_step2.png "Gradients for the second minibatch")
+
+# Event rates?
+The event rates for the first two minibatch are as follows. It seems like the event rates for the layers > 7 are very low/ near zero in the second mini batch, i.e. after the first optimization step.
+
+![Event counts for the first minibatch](doc/img/counts_per_layer_step1.png "Event counts for the first minibatch")
+![Event counts for the second minibatch](doc/img/counts_per_layer_step2.png "Event counts for the second minibatch")
+
+
+
---
+++ dhp19_sdnn_train.py
@@ -0,0 +1,319 @@
+import h5py
+# import numpy as np
+import matplotlib.pyplot as plt
+import os
+import torch
+from torch.utils.data import Dataset, DataLoader
+import torch.nn.functional as F
+import lava.lib.dl.slayer as slayer
+from tqdm import tqdm  # progressbar
+
+class dhp19(Dataset):
+    """
+    Dataset class for the dhp19 dataset
+    """
+
+    def __init__(self, input_tensors, target_tensors, session_tensor, subject_tensor, mov_tensor):
+        self.input_tensors = input_tensors
+        self.target_tensors = target_tensors
+        self.session_tensor = session_tensor
+        self.subject_tensor = subject_tensor
+        self.mov_tensor = mov_tensor
+        
+    def __len__(self):
+        return len(self.input_tensors)
+   
+    def __getitem__(self, idx):
+        return self.input_tensors[idx].to_dense(), self.target_tensors[idx].to_dense(), self.session_tensor[idx], self.subject_tensor[idx], self.mov_tensor[idx]
+        
+
+def plot_sample_from_dataset(input, target, batch_idx, cam_index, time_idx, joint_idx):
+    """
+    Plot a sample from the dataset
+    """
+    input_sample = input[batch_idx, 0, :, :, cam_index, time_idx]
+    target_sample = target[batch_idx, joint_idx, :, :, cam_index, time_idx]  
+    plt_title = f'Input sample from\n batch {batch_idx}, time {time_idx}, cam {cam_index}, target joint {joint_idx}'
+    plt.figure()
+    plt.imshow(input_sample, cmap='gray')
+    plt.imshow(target_sample, alpha=.5)
+    plt.title(plt_title)
+    plt.show()
+
+
+def plot_net_output(output, sample_idx, time_idx, joint_idx):
+    """
+    Plot model output
+    """
+    plt.figure()
+    # arget_sample = target[batch_idx, joint_idx, :, :, cam_index, time_idx]  
+    plt_title = f'Output sample from\n batch {sample_idx}, time {time_idx}, target joint {joint_idx}'
+    plt.imshow(output[sample_idx, joint_idx,:,:,time_idx])
+    plt.title(plt_title)
+    plt.show()
+
+
+# Sparsity loss to penalize the network for high event-rate.
+def event_rate_loss(x, max_rate=0.01):
+    mean_event_rate = torch.mean(torch.abs(x))
+    return F.mse_loss(F.relu(mean_event_rate - max_rate), torch.zeros_like(mean_event_rate))
+
+
+# Define the network
+class Network(torch.nn.Module):
+    """
+    Define the CNN model as SDNN with slayer blocks
+    """
+
+    def __init__(self):
+        super(Network, self).__init__()
+        
+        sdnn_params = { # sigma-delta neuron parameters (taken from PilotNet tutorial)
+                'threshold'     : 0.1,    # delta unit threshold
+                'tau_grad'      : 0.5,    # delta unit surrogate gradient relaxation parameter
+                'scale_grad'    : 1,      # delta unit surrogate gradient scale parameter
+                'requires_grad' : True,   # trainable threshold
+                'shared_param'  : True,   # layer wise threshold
+                'activation'    : F.relu, # activation function
+            }
+        
+        sdnn_cnn_params = { # conv layer has additional mean only batch norm
+                **sdnn_params,                                 # copy all sdnn_params
+                'norm' : slayer.neuron.norm.MeanOnlyBatchNorm, # mean only quantized batch normalizaton
+            }
+        # sdnn_dense_params = { # dense layers have additional dropout units enabled
+        #         **sdnn_cnn_params,                        # copy all sdnn_cnn_params
+        #         'dropout' : slayer.neuron.Dropout(p=0.2), # neuron dropout
+        #     }
+        
+        self.blocks = torch.nn.ModuleList([# sequential network blocks 
+                # delta encoding of the input
+                slayer.block.sigma_delta.Input(sdnn_params, bias=None), 
+                
+                # 1. convolution layer 1 stride 1, dilitation 1, max pooling 2x2
+                slayer.block.sigma_delta.Conv(sdnn_cnn_params, in_features=1, out_features=16, kernel_size=3, stride=1, padding=1, dilation=1, weight_scale=2, weight_norm=True),
+                # torch.Size([1, 16, 260, 344, 16])
+                
+                # pool 1 mean pooling 2x2 (original architectur uses max pooling)
+                slayer.block.sigma_delta.Pool(sdnn_params, kernel_size=2),
+                # torch.Size([1, 16, 130, 172, 16])
+                
+                # 2. convolution layer stride 1, dilitation 1,
+                slayer.block.sigma_delta.Conv(sdnn_cnn_params, in_features=16, out_features=32, kernel_size=3, stride=1, padding=1, dilation=1, weight_scale=2, weight_norm=True),
+                # torch.Size([1, 32, 130, 172, 16])
+
+                # 3. convolution layer stride 1, dilitation 1,
+                slayer.block.sigma_delta.Conv(sdnn_cnn_params, in_features=32, out_features=32, kernel_size=3, stride=1, padding=1, dilation=1, weight_scale=2, weight_norm=True),
+                # torch.Size([1, 32, 130, 172, 16])
+
+                # 4. convolution layer stride 1, dilitation 1,
+                slayer.block.sigma_delta.Conv(sdnn_cnn_params, in_features=32, out_features=32, kernel_size=3, stride=1, padding=1, dilation=1, weight_scale=2, weight_norm=True),
+                # torch.Size([1, 32, 130, 172, 16])
+                
+                # pool 2 mean pooling 2x2 (original architectur uses max pooling)
+                slayer.block.sigma_delta.Pool(sdnn_params, kernel_size=2),
+                # torch.Size([1, 32, 65, 86, 16])
+                
+                # 5. convolution layer stride 1, dilitation 2,
+                slayer.block.sigma_delta.Conv(sdnn_cnn_params, in_features=32, out_features=64, kernel_size=3, stride=1, padding=2, dilation=2, weight_scale=2, weight_norm=True),
+                # torch.Size([1, 64, 65, 86, 16])
+                
+                # 6. convolution layer stride 1, dilitation 2,
+                slayer.block.sigma_delta.Conv(sdnn_cnn_params, in_features=64, out_features=64, kernel_size=3, stride=1, padding=2, dilation=2, weight_scale=2, weight_norm=True),
+                # torch.Size([1, 64, 65, 86, 16])
+                
+                # 7. convolution layer stride 1, dilitation 2,
+                slayer.block.sigma_delta.Conv(sdnn_cnn_params, in_features=64, out_features=64, kernel_size=3, stride=1, padding=2, dilation=2, weight_scale=2, weight_norm=True),
+                # torch.Size([1, 64, 65, 86, 16])
+                
+                # 8. convolution layer stride 1, dilitation 2,
+                slayer.block.sigma_delta.Conv(sdnn_cnn_params, in_features=64, out_features=64, kernel_size=3, stride=1, padding=2, dilation=2, weight_scale=2, weight_norm=True),
+                # torch.Size([1, 64, 65, 86, 16])
+                
+                # 9. transposed convolution layer stride 2, dilitation 1, (using a 2x2 kernel instead of 3x3 as reportet in the paper to get correct output dims)
+                slayer.block.sigma_delta.ConvT(sdnn_cnn_params, in_features=64, out_features=32, kernel_size=2, stride=2, padding=0, dilation=1, weight_scale=2, weight_norm=True),
+                # torch.Size([1, 32, 130, 172, 16])
+
+                # 10. convolution layer stride 1, dilitation 2,
+                slayer.block.sigma_delta.Conv(sdnn_cnn_params, in_features=32, out_features=32, kernel_size=3, stride=1, padding=2, dilation=2, weight_scale=2, weight_norm=True),
+                # torch.Size([1, 32, 130, 172, 16])
+                
+                # 11. convolution layer stride 1, dilitation 2,
+                slayer.block.sigma_delta.Conv(sdnn_cnn_params, in_features=32, out_features=32, kernel_size=3, stride=1, padding=2, dilation=2, weight_scale=2, weight_norm=True),
+                # torch.Size([1, 32, 130, 172, 16])
+
+                # 12. convolution layer stride 1, dilitation 2,
+                slayer.block.sigma_delta.Conv(sdnn_cnn_params, in_features=32, out_features=32, kernel_size=3, stride=1, padding=2, dilation=2, weight_scale=2, weight_norm=True),
+                # torch.Size([1, 32, 130, 172, 16])
+
+                # 13. convolution layer stride 1, dilitation 2,
+                slayer.block.sigma_delta.Conv(sdnn_cnn_params, in_features=32, out_features=32, kernel_size=3, stride=1, padding=2, dilation=2, weight_scale=2, weight_norm=True),
+                # torch.Size([1, 32, 130, 172, 16])
+
+                # 14. transposed convolution layer stride 2, dilitation 1, (using a 2x2 kernel instead of 3x3 as reportet in the paper to get correct output dims)
+                slayer.block.sigma_delta.ConvT(sdnn_cnn_params, in_features=32, out_features=16, kernel_size=2, stride=2, padding=0, dilation=1, weight_scale=2, weight_norm=True),
+                # torch.Size([1, 16, 260, 344, 16])
+
+                # 15. convolution layer stride 1, dilitation 1,
+                slayer.block.sigma_delta.Conv(sdnn_cnn_params, in_features=16, out_features=16, kernel_size=3, stride=1, padding=1, dilation=1, weight_scale=2, weight_norm=True),
+                # torch.Size([1, 16, 130, 172, 16])
+
+                # 16. convolution layer stride 1, dilitation 1,
+                slayer.block.sigma_delta.Conv(sdnn_cnn_params, in_features=16, out_features=16, kernel_size=3, stride=1, padding=1, dilation=1, weight_scale=2, weight_norm=True),
+                # torch.Size([1, 16, 130, 172, 16])
+
+                # 17. convolution layer stride 1, dilitation 1, out_features = number of heatmaps to predict
+                slayer.block.sigma_delta.Conv(sdnn_cnn_params, in_features=16, out_features=13, kernel_size=3, stride=1, padding=1, dilation=1, weight_scale=2, weight_norm=True)
+                # torch.Size([1, 13, 130, 172, 16])
+        ])  
+        
+
+    def forward(self, x):
+        count = []
+        event_cost = 0
+
+        for block in self.blocks: 
+            # forward computation is as simple as calling the blocks in a loop
+            x = block(x)
+            if hasattr(block, 'neuron'):
+                event_cost += event_rate_loss(x)
+                count.append(torch.sum(torch.abs((x[..., 1:]) > 0).to(x.dtype)).item())
+
+        return x, event_cost, torch.FloatTensor(count).reshape((1, -1)).to(x.device)
+
+
+    def grad_flow(self, path=None):
+        # helps monitor the gradient flow
+        grad = [b.synapse.grad_norm for b in self.blocks if hasattr(b, 'synapse')]
+
+        plt.figure()
+        plt.semilogy(grad)
+        if path:
+            plt.savefig(path + 'gradFlow.png')
+            plt.close()
+        else:
+            plt.show()
+
+        return grad
+
+    def export_hdf5(self, filename):
+        # network export to hdf5 format
+        h = h5py.File(filename, 'w')
+        layer = h.create_group('layer')
+        for i, b in enumerate(self.blocks):
+            b.export_hdf5(layer.create_group(f'{i}'))
+
+
+def load_file_(filepath):
+    """
+    Load data from a .pt file.
+    :param filepath: path to the file
+    :return: data from the file
+    """
+    if filepath.endswith('.pt'):
+        data = torch.load(filepath)
+    else:
+        raise ValueError('.pt required format.')
+    return data
+
+
+# path to dhp19 eventframes
+event_data_path = './data/'
+project_path = './'
+
+# training parameters
+batch_size  = 8  # batch size
+learning_rate = 0.001 # leaerning rate
+lam    = 0.01  # lagrangian for event rate loss
+num_epochs = 10  # training epochs
+# steps  = [60, 120, 160] # learning rate reduction milestones
+cam_index = 1 # camera index to train on
+seq_length = 8 # number of event frames per sequence to be shown to the SDNN
+
+model_name = 'dhp19_sdnn'
+device = torch.device('cuda:0')
+
+# create experiment name
+experiment_name = model_name + \
+                '_epochs' + str(num_epochs) + \
+                '_lr' + str(learning_rate) + \
+                '_batchsize' + str(batch_size) + \
+                '_seq' + str(seq_length) + \
+                '_cam' + str(cam_index) + \
+                '_lam' + str(lam)
+
+print('Starting experiment:', experiment_name)
+
+#load data
+data_dict = torch.load(event_data_path + f'dhp19_dataset_sparse_small_seq{seq_length}.pt')
+# data_dict['input_tensors'].shape # N T C H W Cam format
+
+# Instantiate Network, Optimizer, Dataset and Dataloader
+# define dataset
+dhp19_dataset = dhp19(data_dict['input_tensors'],
+                        data_dict['target_tensors'],
+                        data_dict['session_tensor'],
+                        data_dict['subject_tensor'],
+                        data_dict['mov_tensor'])
+
+train_loader = DataLoader(dataset=dhp19_dataset, batch_size=8, shuffle=True, num_workers=12)
+# dhp19_dataset.__len__()
+
+# plot some samples
+input, target, session, subject, mov = next(iter((train_loader)))
+# input.shape
+# target.shape
+batch_idx = 0
+cam_index = 1
+time_idx = 0
+joint_idx = 8
+plot_sample_from_dataset(input, target, batch_idx, cam_index, time_idx, joint_idx)
+
+model = Network().to(device)
+optimizer = torch.optim.RMSprop(model.parameters(), lr=learning_rate)
+
+# training loop
+for epoch in range(num_epochs):
+    # train one epoch
+    running_loss = 0.0        
+    batch_loss = []
+    model.train()
+    epoch_train_loop = tqdm(train_loader)  # setup progress bar
+
+    for batch_idx, (input, target, session, subject, mov) in enumerate(epoch_train_loop): # training loop
+
+        input = input[:,:,:,:,cam_index,:].to(device) # N C H W T
+        # target.shape, # N, C, H, W, Cam T format
+        target = target[:,:,:,:,cam_index,:].to(device) # N C H W T   
+        
+        # zero the parameter gradient
+        optimizer.zero_grad()
+        # forward inputs
+        output, event_cost, count = model(input)
+        
+        # compute loss + backward + optimize
+        loss = F.mse_loss(output, target)
+        loss += lam * event_cost
+        loss.backward()
+        optimizer.step()
+
+        # grads = model.grad_flow()
+        # plt.plot(grads)
+
+        # store loss
+        batch_loss.append(loss.item())
+
+        epoch_train_loop.set_description(f'Epoch [{epoch}/{num_epochs}]')
+        epoch_train_loop.set_postfix()
+
+        # compute scores for the epoch
+        running_loss += loss.item() * input.size(0)
+
+    epoch_loss = running_loss / dhp19_dataset.__len__()
+    # show metrics for epoch
+    print('Loss: {:.4f} '.format(epoch_loss), end=' ')
+    # model.grad_flow(act_result_path + 'plots/')
+
+    # # checkpoint saves
+    # if epoch%1 == 0:
+    #     torch.save({'net': net.state_dict(), 'optimizer': optimizer.state_dict()}, act_result_path + f'checkpoint{epoch}.pt')                   
